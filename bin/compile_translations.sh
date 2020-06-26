9b54cf8d0f8d87a16c8fbeee6d7176f1a7157694
Breyten Ernsting <bje@dds.nl>
2020-06-26 11:25:50 +0200

    Some more formatting.

--- bin/compile_translations.sh
+++ bin/compile_translations.sh
@@ -4,4 +4,6 @@ BINDIR=`dirname $FQPATH`
 docker exec -it alt_app_1 bash -c 'cd frontend && ./compile-translations.sh'
 cd $BINDIR/../app/frontend/translations
 find . -name '*.po' -exec sudo chown "$USER.$USER" \{\} \;
+sudo chown "$USER.$USER" $BINDIR/../app/frontend/translations/??
+sudo chown "$USER.$USER" $BINDIR/../app/frontend/translations/??/LC_MESSAGES
 cd -
