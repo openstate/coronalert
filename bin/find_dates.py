645e59189eb978b5e1a4087f351c55a6ad50c0a2
Breyten Ernsting <bje@dds.nl>
2020-07-09 12:15:09 +0200

    More improved date handling.

--- bin/find_dates.py
+++ bin/find_dates.py
@@ -16,7 +16,7 @@ sys.path.insert(0, '.')
 from ocd_backend.settings import HTML_PATH
 
 def extract_date_from_meta(html):
-    parsed_granularity = 12
+    parsed_granularity =12
     date_str = None
     # meta_terms = ['DCTERMS.modified', 'dcterms:modified']
     meta_terms = ['DCTERMS.available', 'dcterms:available']
@@ -43,11 +43,55 @@ def extract_iso8601_date(contents):
             parsed_granularity = 0
     return parsed_date, parsed_granularity
 
+def extract_text_date(contents):
+    parsed_granularity = 12
+    parsed_date = None
+
+    # assumes text
+    date_str = contents
+    date_match = re.search(r'(?P<d>\d{1,2})-(?P<m>\d{2})-(?P<y>\d{4})', date_str)
+    if not date_match:
+        date_match = re.search((
+            r'(?P<d>\d+)\s+(?P<m>januari|februari|maart|april|mei|juni|juli|augustus|'
+            r'september|oktober|november|december)\s+(?P<y>\d{4})'), date_str)
+    if not date_match:
+        date_match = re.search((
+            r'(?P<d>\d+)\s+(?P<m>jan\.|feb\.|mar\.|apr\.|mei|jun\.|jul\.|aug\.|'
+            r'sep\.|okt\.|nov\.|dec\.)\s+(?P<y>\d{4})'), date_str)
+    if date_match is not None:
+        date_conversions = {
+            'januari': '01', 'februari': '02', 'maart': '03',
+            'april': '04', 'mei': '05', 'juni': '06', 'juli': '07',
+            'augustus': '08', 'september': '09', 'oktober': '10',
+            'november': '11', 'december': '12', 'jan.': '01',
+            'feb.': '02', 'mar.': '03', 'apr.': '04', 'mei': '05',
+            'jun.': '06', 'jul.': '07', 'aug.': '08', 'sep.': '09',
+            'okt.': '10', 'nov.': '11', 'dec.': '12'}
+        if len(date_match.group('d')) <= 1:
+            date_prefix = '' if date_match.group('d').startswith('0') else '0'
+        else:
+            date_prefix = ''
+        mnth = date_conversions.get(
+            date_match.group('m'), date_match.group('m'))
+        date_semi_parsed = u'%s-%s-%s%sT00:00:00' % (
+            date_match.group('y'), mnth,
+            date_prefix, date_match.group('d'),)
+        try:
+            parsed_date = iso8601.parse_date(
+                date_semi_parsed)
+            parsed_granularity = 12
+        except LookupError:
+            parsed_granularity = 0
+            pass
+    return parsed_date, parsed_granularity
 
 def extract_date(html, contents):
     parsed_date, parsed_granularity = extract_date_from_meta(html)
     if parsed_date is None:
         parsed_date, parsed_granularity = extract_iso8601_date(contents)
+    if parsed_date is None:
+        parsed_date, parsed_granularity = extract_text_date(contents)
+
     return parsed_date, parsed_granularity
 
 def main(argv):
