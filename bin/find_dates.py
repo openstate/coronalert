ca152fa114534a944c7185457ce0541aff6a3d31
Breyten Ernsting <bje@dds.nl>
2020-07-09 11:51:52 +0200

    Some improvements in date parsing.

--- bin/find_dates.py
+++ bin/find_dates.py
@@ -15,7 +15,7 @@ sys.path.insert(0, '.')
 
 from ocd_backend.settings import HTML_PATH
 
-def _extract_date(html=None):
+def extract_date_from_meta(html):
     parsed_granularity = 12
     date_str = None
     # meta_terms = ['DCTERMS.modified', 'dcterms:modified']
@@ -31,13 +31,31 @@ def _extract_date(html=None):
         parsed_granularity = 0
     return parsed_date, parsed_granularity
 
+def extract_iso8601_date(contents):
+    parsed_granularity = 12
+    parsed_date = None
+    matches = re.search(r'(\d{4}-\d{2}\\d{2}T\d{2}:\d{2}:\d{2})', contents)
+    if matches is not None:
+        try:
+            parsed_date = iso8601.parse_date(matches.group(1))
+        except iso8601.ParseError:
+            parsed_date = None
+            parsed_granularity = 0
+    return parsed_date, parsed_granularity
+
+
+def extract_date(html, contents):
+    parsed_date, parsed_granularity = extract_date_from_meta(html)
+    if parsed_date is None:
+        parsed_date, parsed_granularity = extract_iso8601_date(contents)
+    return parsed_date, parsed_granularity
 
 def main(argv):
     for h in glob(os.path.join(HTML_PATH, '*.html')):
         with open(h, 'r') as in_file:
             contents = in_file.read()
         html = etree.HTML(contents)
-        d, g = _extract_date(html)
+        d, g = extract_date(html, contents)
         print("%s: %s / %s" % (h, g, d,))
     return 0
 
